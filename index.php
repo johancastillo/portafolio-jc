c8ce0b630754ac63bba48450ab371f711b88d834
Johan Castillo <jcjohan2707@gmail.com>
2020-12-30 18:22:14 -0400

    Particles with JS

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 <body>
+    <div id="particles-js"></div>
+
     <!-- **** Navbar Start **** -->
     <?php include "./components/Navbar.php" ?>
     <!-- **** Navbar End **** -->
@@ -37,5 +39,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 
+    <!-- Particles JS -->
+    <script src="./assets/js/particles.min.js"></script>
+    <script src="./assets/js/app.js"></script>
 </body>
 </html>
